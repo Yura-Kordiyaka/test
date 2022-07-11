d9af653a7bec1a2fb6345e23a9add639b3139a8d
Yura <96499300+yurarasik@users.noreply.github.com>
2022-07-11 20:44:04 +0300

    week 4 exception

--- main.py
+++ main.py
@@ -1,30 +1,25 @@
-#week3task3
-def remove_coma(a):
-    for j in a:
-        if j == ",":
-            a.remove(j)
+from user_functions import *
 
-
-def add_element(a, c):
-    for j in range(0, len(c)):
-        a.append(c[j])
-
-
-t = open("test.txt", "r")
-w = open("write.txt", "w")
-f = t.read()
-d = []
-z = ()
-add_element(d, f)
-remove_coma(d)
-z = list(z)
-z = d
-z = tuple(z)
-print(d)
-print(z)
-for j in d:
-    w.write(j+" ")
-for j in z:
-    w.write(j+" ")
-
-
+while True:
+    print("1. Add New User\n"
+          + "2. Get All Users\n"
+          + "3. Search\n"
+          + "4. Update User By Id\n"
+          + "5 if you want exit"
+          )
+    menu_flag = int(input("Type your choose: "))
+    if menu_flag == 1:
+        user_add()
+    elif menu_flag == 2:
+        try:
+            get_all()
+        except UnboundLocalError:
+            print("incorrect data")
+    elif menu_flag == 3:
+        what_to_search = input('By Which Parametr you want to search: ')
+        search_str = input('Search: ')
+        search_by(search_str, what_to_search)
+    elif menu_flag == 4:
+        update_user()
+    elif menu_flag == 5:
+        break
